9b1448876697958a2afd88e4c4b7969f42b944e9
Tristan Sahagun <t.sahagun@students.clark.edu>
2020-05-13 12:17:07 -0700

    Updated as following lecture

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+<Tristan Sahagun>
+<Module 4 Lab 2>
 <assignment/lab description
 """
 
@@ -10,8 +10,26 @@ Input(s): list/description
 Process: description of what function does
 Output: return value and description
 """
-
+from graphics import *
 def main():
-    # code here
+    '''
+    startValue = 1
+    endValue = 10
+    sum = 0
+    if startValue % 2 == 1:
+        startValue = startValue + 1
+    for i in range(startValue, endValue, 2):
+        sum = sum + 1
+    print(sum)
+    '''
+    win = GraphWin("demo", 800, 800)
+    win.setCoords(-4.0, - 4.0, 4.0, 4.0)
+    p1 = Circle(Point(2, 3), 0.1)
+    p1.setFill("red")
+    p1.draw(win)
+    p2 = Point(-3, 1).draw(win)
+    p3 = Point(-1.5, -2.5).draw(win)
+
+    input()
 
 main()    
